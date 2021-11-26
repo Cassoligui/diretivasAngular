495f79c95915fd4a02c3f60e144bd60665e784b1
guilherme souza <cassoligui@gmail.com>
2021-11-26 10:01:48 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ambiente-angular';
+  count = 0;
+  text = "";
+  pessoas = [
+    {
+      nome: "Guilherme",
+      sobrenome: "Cassoli"
+    },
+    {
+      nome: "Maria",
+      sobrenome: "Silva"
+    },
+    {
+      nome: "José",
+      sobrenome: "Souza"
+    },
+    {
+      nome: "Marcio",
+      sobrenome: "Santos"
+    }
+
+  ];
+
+  constructor() {
+
+  }
+  ngOnInit() {
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+  clicou(): void {
+    console.log('Clicou em mim')
+  }
 }
+
